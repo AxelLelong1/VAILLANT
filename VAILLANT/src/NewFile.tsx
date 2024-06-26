26f958788558dd31bd88152149607c9c190c271b
axel.lelong <axel.lelong@outlook.com>
2024-06-26 13:14:49 +0200

    case sensitive be careful

--- VAILLANT/src/NewFile.tsx
+++ VAILLANT/src/NewFile.tsx
@@ -11,7 +11,7 @@ const FileCreationInput: React.FC<FileOpeningInputProps> = ({ onFileCreation }) 
         if (files && files.length > 0) {
             console.log(files[0])
             const folderPath = files[0].webkitRelativePath?.split('/')[0] ?? '';
-            onFileCreation("../projets/" + folderPath);
+            onFileCreation("../Projets/" + folderPath);
         }
     };
 
