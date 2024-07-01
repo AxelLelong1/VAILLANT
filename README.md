57ae8e2dcf430362705129768206e2dd25685b11
axel.lelong <axel.lelong@outlook.com>
2024-07-01 10:18:47 +0200

    SH IS FINE + readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Instructions pour Lancer l'IDE
+
+## Étape 1 : Lancer le Script
+
+Pour démarrer l'IDE, il vous suffit de lancer le script correspondant à votre système d'exploitation :
+
+- **Windows** : Double-cliquez sur le fichier `run.bat`.
+- **Linux/MacOS** : Exécutez le script `run.sh` depuis un terminal.
+
+## Étape 2 : Attendre le Démarrage de l'IDE
+
+Après avoir lancé le script, l'IDE devrait se lancer automatiquement après un court instant. Patientez quelques instants pendant que le programme se charge.
+
+## Étape 3 : Utilisation de l'IDE
+
+Une fois l'IDE lancé, il est prêt à être utilisé. Vous pouvez maintenant commencer à travailler sur vos projets.
+
+## Besoin d'Aide ?
+
+Si vous avez besoin d'aide ou de plus d'informations sur l'utilisation de l'IDE, n'hésitez pas à consulter l'onglet **"AIDE"** dans l'application.
--- run.sh
+++ run.sh
@@ -24,19 +24,67 @@ check_install_ruby() {
     fi
 }
 
+# Function to handle cleanup on exit
+cleanup() {
+    echo "Cleaning up..."
+    kill -9 $BACKEND_PID $FRONTEND_PID
+    exit 0
+}
+
+# Set trap to call cleanup on script exit
+trap cleanup SIGINT SIGTERM
+
 # Check and install Python3 and Ruby
 check_install_python
 check_install_ruby
 
 # Start the backend server
-java -jar APP/quarkus-app/quarkus-run.jar
+java -jar APP/quarkus-app/quarkus-run.jar &
+BACKEND_PID=$!
+
+sleep 5
 
 # Serve the frontend files
 cd APP/VAILLANT
-python3 -m http.server 1573
-
+python3 -m http.server 1573 &
+FRONTEND_PID=$!
+cd -
 # Wait for a few seconds to ensure the server is up and running
 sleep 5
 
 # Open the default web browser
 xdg-open http://localhost:1573
+# Inline Python script to monitor the web browser
+python3 - <<EOF &
+import time
+import psutil
+import webbrowser
+
+url = "http://localhost:1573"
+
+def is_browser_open(port):
+    for proc in psutil.process_iter(['pid', 'name']):
+        try:
+            if 'firefox' in proc.info['name'] or 'chrome' in proc.info['name']:
+                for conn in proc.connections(kind='inet'):
+                    if conn.laddr.port == port:
+                        return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False
+
+#webbrowser.open(url)
+while True:
+    if not is_browser_open(1573):
+        break
+    time.sleep(2)
+
+print("Browser closed, exiting...")
+EOF
+BROWSER_MONITOR_PID=$!
+
+# Wait for the Python script to finish
+wait $BROWSER_MONITOR_PID
+
+# Cleanup servers when browser is closed
+cleanup
